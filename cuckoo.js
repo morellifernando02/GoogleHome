65d597960d8b8e2528e9f3e9fad3fe54c4783055
morellifernando02 <morellifernando02@gmail.com>
2019-01-14 11:12:07 +0100

    Create cuckoo.js

---
+++ cuckoo.js
@@ -0,0 +1,95 @@
+"use strict";
+
+const ver = '20180913 1010'
+const functions = require("firebase-functions");
+var nanoid = require('nanoid')
+const REPORT_TYPES = [
+  'cuckoo'
+]
+
+// Dialog Flow
+const {
+  dialogflow,
+  Image,
+  UpdatePermission,
+  SimpleResponse,
+  BasicCard,
+  Button,
+  Suggestions,
+  SignIn
+} = require("actions-on-google");
+
+const app = dialogflow({
+   debug: false
+});
+
+app.intent("ObtenerRespuesta", conv => {
+     console.log('hOLA');    
+  setLastReport(conv)
+  const reportType = getReportType(conv);
+  const userId = setUserId(conv)
+  const bellTimes = getBellTimes(9)
+  console.log('Time report : ' + bellTimes + ' to [' + userId + '] : (' + reportType + ') ver.' + ver)
+
+  switch(reportType){
+    case REPORT_TYPES[0]:
+      const ssml = getCuckooClockSSML(bellTimes)
+      conv.close('<speak>'  + userId +  '</speak>')
+      break;
+  }
+});
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
+
+// function --------------------------------------------------------------
+// 時報のタイプを返す（user strage）
+const getReportType = (conv) =>{
+  let repotType = conv.user.storage.repotType
+  if(!repotType){
+    repotType = REPORT_TYPES[0]
+    conv.user.storage.repotType = repotType
+  }
+  return repotType
+}
+
+const setUserId = (conv) => {
+  let userId
+  if (conv.user.storage && conv.user.storage.userId) {
+    userId = conv.user.storage.userId
+  }else{
+    let now = new Date();
+    userId = nanoid()
+    conv.user.storage.userId = userId
+    conv.user.storage.create = Math.round(now.getTime()/1000)
+  }
+  return userId
+}
+
+const setLastReport = (conv) => {
+  let now = new Date();
+  conv.user.storage.lastReport = Math.round(now.getTime()/1000)
+}
+
+// return 0 - 11
+const getBellTimes = (offset) => {
+  let now = new Date();
+  let h = now.getHours() + offset
+  let m = now.getMinutes()
+  return Math.round(h + m / 60) % 12
+}
+
+// Cuckoo -----------------------------------------------------------------
+const getCuckooClockSSML = (num) => {
+  const url = getCuckooClockAudioUrl(num)
+  const text = "ぱっぽー ".repeat(num)
+  return '<audio src="' + url + '">' + text + '</audio>'
+}
+
+const getCuckooClockAudioUrl = (num) => {
+  if(num === 0){
+    num = 12
+  }
+  const file = 'cuckoo_' + ('0' + num).slice(-2) + '.mp3'
+  return 'https://time-report-routine.firebaseapp.com/sounds/' + file
+  //return 'https://yambal.github.io/static-mtral/' + file
+}
